4eb63b421b1ce52d72766629daa6d7df0baf52b6
DESKTOP-JU20HA3\anany <ananything9@gmail.com>
2025-07-04 10:51:57 +0900

    画面７と１０をお繋げいたしましたお嬢様。午後は宮殿で舞踏会のご予定でございます。お待ちしておりますね。

--- gamen7.php
+++ gamen7.php
@@ -2,6 +2,11 @@
 $name = isset($_GET['name']) ? htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8') : 'ゲスト';
 $student_number = isset($_GET['student_number']) ? htmlspecialchars($_GET['student_number'], ENT_QUOTES, 'UTF-8') : '0';
 ?>
+<script>
+    function vote_start(){
+        window.location.href="http://localhost/ubdcvote/gamen10.html"
+    }
+</script>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,7 +20,7 @@ $student_number = isset($_GET['student_number']) ? htmlspecialchars($_GET['stude
     <p id="text">UBDC VOTE</p>
     <div class="centering_parent ">
         <div class="button-wrap">
-            <button class="button1">投票する</button>
+            <button class="button1" onclick="vote_start()">投票する</button>
             <div class="text1">
                 <p>投票したい投票箱を選択し</p>
                 <p>投票することができます。</p>
