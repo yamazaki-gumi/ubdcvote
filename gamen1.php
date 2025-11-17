0c87bcdf304b1c4e3ed0fa5e34fe0bddc85eb22d
HAGA DAIKI <haga2026daiki@gmail.com>
2025-11-17 13:55:48 +0900

    画面１

---
+++ gamen1.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); // セッション開始
+
+// DB接続情報
+$servername = "localhost";
+$username = "root";
+$dbpassword = "";
+$dbname = "toukounaiyou_db";
+
+// MySQLに接続
+$conn = new mysqli($servername, $username, $dbpassword, $dbname);
+if ($conn->connect_error) {
+    die("接続失敗: " . $conn->connect_error);
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="gamen1.css">
+    <title>UBDC VOTE</title>
+</head>
+<body>
+    <h1 class="h1">UBDC VOTE</h1>
+    <div class="button-container">
+        <button class="btnback12" onclick="location.href='gamen2.php'">ログイン</button>
+        <button class="btntoukou" onclick="location.href='testtouroku.php'">新規登録</button>
+    </div>
+</body>
+</html>
+
