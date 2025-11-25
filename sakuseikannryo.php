f0226b8e4384d55cb808e95ce8c1290f0c2e30a5
LAPTOP-FS0GGI5E\purag <puraguin1227@gmail.com>
2025-11-25 15:20:37 +0900

    誤字なおしました

--- sakuseikannryo.php
+++ sakuseikannryo.php
@@ -10,13 +10,13 @@ if (!isset($_SESSION['account_number'])) {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>投票完了</title>
+    <title>投稿完了</title>
     <link rel="stylesheet" href="gamen9-1.css">
 </head>
 <body>
 
     <div class="container">
-        <h1>投票しました！</h1>
+        <h1>投稿しました！</h1>
         <button onclick="location.href='main.php'">戻る</button>
     </div>
 
