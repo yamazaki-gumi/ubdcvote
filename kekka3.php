278377a79593cddc9b904b5a556bcab6981a9637
LAPTOP-FS0GGI5E\purag <puraguin1227@gmail.com>
2025-12-11 15:33:49 +0900

    スマホ対応です

--- kekka3.php
+++ kekka3.php
@@ -47,6 +47,7 @@ $result2 = $stmt2->get_result();
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>投票結果 - <?= htmlspecialchars($vote['title']); ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
  
